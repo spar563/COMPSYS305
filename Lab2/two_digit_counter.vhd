e8f15a4448a126b4988ec1c89bf5d0e2d960eac3
spar563 <126215847+spar563@users.noreply.github.com>
2024-03-28 15:15:05 +1300

    Lab2: two_digit_counter using components

--- Lab2/two_digit_counter.vhd
+++ Lab2/two_digit_counter.vhd
@@ -4,37 +4,51 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 
 entity two_digit_counter is
-	Port (
+Port (
         Clk : in STD_LOGIC;
         Reset : in STD_LOGIC;
         Enable : in STD_LOGIC;
         Q : out STD_LOGIC_VECTOR (7 downto 0)
-	);
+);
 end two_digit_counter;
 
 architecture Behaviour of two_digit_counter is
-    signal cntr : integer range 00 to 99 := 0;
+
+component BCD_counter
+Port (
+        Clk : in STD_LOGIC;
+        Direction : in STD_LOGIC;
+        Init : in STD_LOGIC;
+        Enable : in STD_LOGIC;
+        Q_Out : out STD_LOGIC_VECTOR (3 downto 0)
+);
+end component;
+
+signal ones_place, tens_place : std_logic_vector(3 downto 0);
+signal tens_enable : std_logic;
+
 begin
-	process (Clk, Reset)
-	begin
-		if Reset = '1' then
-			cntr <= 99;
-		else
 
-		if rising_edge (clk) then
-			if Enable = '1' then
-				if cntr > 0 then
-					-- up counter
-					cntr <= cntr - 1;
-				else 
-					-- set counter to 0 once it reaches 9
-					cntr <= 99;
-				end if;
-			end if;
-		end if;
+BCD_Ones : BCD_counter port map (
+			Clk => Clk,
+			Direction => '1',
+			Init => Reset,
+			Enable => Enable,
+			Q_Out => ones_place );
 
-	end if;
-	end process;
-    Q <= conv_std_logic_vector(cntr, Q'length);
+BCD_Tens : BCD_counter port map (
+			Clk => Clk,
+			Direction => '1',
+			Init => Reset,
+    			Enable => tens_enable,
+			Q_Out => tens_place );
+
+
+tens_enable <= '1' when (ones_place = "0000") and (Enable = '1') else '0';
+
+
+
+Q <= tens_place & ones_place;
+
 end Behaviour;
-	
+
